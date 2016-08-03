745b544957d3d9ee9ccd79fedf53fe1c8dc873d7
applqpak <applqpak@gmail.com>
2016-08-03 14:57:40 +1000

    Update Main.java

--- hm/applqpak/HelpModifier/Main.java
+++ hm/applqpak/HelpModifier/Main.java
@@ -11,6 +11,26 @@ public class Main extends PluginBase
 
   public Config config;
 
+  public String[] toS(List lst)
+  {
+
+    String[] strArray = new String[lst.size()];
+
+    int index = 0;
+
+    for(Object value : lst)
+    {
+
+      strArray[index] = String.valueOf(value);
+
+      index++;
+
+    }
+
+    return strArray;
+
+  }
+
   @Override
 
   public void onEnable()
